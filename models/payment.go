6eb1fc6d7de1c92e556c70fe8fffaedb46539263
Huong3203 <100497937+Huong3203@users.noreply.github.com>
2025-11-19 05:34:28 +0700

    Upload project lên GitHub

--- models/payment.go
+++ models/payment.go
@@ -6,10 +6,11 @@ import (
 
 type Payment struct {
 	ID        string    `gorm:"type:char(36);primaryKey" json:"id"`
-	OrderID   string    `gorm:"type:varchar(50);index"   json:"order_id"` // orderId MoMo
-	UserID    string    `gorm:"type:char(36);not null"   json:"user_id"`  // user mua gói VIP
+	OrderID   string    `gorm:"type:varchar(50);index"   json:"order_id"`
+	UserID    string    `gorm:"type:char(36);not null"   json:"user_id"`
 	Amount    int       `gorm:"type:int;not null"        json:"amount"`
-	Status    string    `gorm:"type:varchar(20);default:'pending'" json:"status"` // pending, success, failed
+	Status    string    `gorm:"type:varchar(20);default:'pending'" json:"status"`
+	PodcastID *string   `gorm:"type:char(36);default:NULL" json:"podcast_id"` // VIP payment để nil
 	CreatedAt time.Time `gorm:"autoCreateTime"           json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"           json:"updated_at"`
 }
